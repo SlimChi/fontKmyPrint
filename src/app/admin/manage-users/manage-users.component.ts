661d9a51e31b5580323014068e0ad81119d18823
Slimane <slimane.chihati@gmail.com>
2023-04-08 07:56:01 +0200

    slimane commit

--- src/app/admin/manage-users/manage-users.component.ts
+++ src/app/admin/manage-users/manage-users.component.ts
@@ -5,15 +5,17 @@ import { HelperService } from '../../services/helper/helper.service';
 import {AddressService} from "../../swagger/services/services/address.service";
 import {AdresseDto} from "../../swagger/services/models/adresse-dto";
 import {TypeAdresse} from "../../swagger/services/models/type-adresse";
-import {forkJoin} from "rxjs";
+import {forkJoin, Observable} from "rxjs";
 
 @Component({
   selector: 'app-manage-users',
   templateUrl: './manage-users.component.html',
+  styleUrls: ['./manage-users.component.css']
 })
 export class ManageUsersComponent implements OnInit {
   users: Array<UtilisateurDto> = [];
-  selectedUser: UtilisateurDto | null = null; // Initialize with null to avoid TS2322
+  selectedUser: UtilisateurDto | null = null;
+
   adresse: Array<AdresseDto> = [];
   showDetail = false;
   typesAdresse: Array<TypeAdresse> = []; // étape 1
@@ -28,12 +30,10 @@ export class ManageUsersComponent implements OnInit {
 
   ngOnInit(): void {
     this.findAllusers();
-    this.getAllTypeAdresse()
-    console.log(this.findAllusers());
-    console.log(this.findById());
-    console.log(this.selectedUser)
-    console.log(this.getAllTypeAdresse())
-
+    this.getAllTypeAdresse().subscribe(() => {
+      console.log(this.typesAdresse);
+    });
+    this.findById();
   }
 
   private findAllusers() {
@@ -51,34 +51,53 @@ export class ManageUsersComponent implements OnInit {
     this.getAllTypeAdresse()
   }
 
-  private findById() {
-    this.userService.getUtilisateurById({
-      "idUtilisateur": this.helperService.userId})
-    this.adresseService.findAll1()
-    if (this.helpService.userId) {
-      this.adresseService.findAll1(this.helpService.userId).subscribe({
-        next: (adresses) => {
-          this.adresse = adresses.filter((adresse) => adresse.utilisateurId === this.helpService.userId);
-          console.log(this.adresse)
-        },
-        error: (err) => {
-          console.error(err);
-          // gérer le cas d'erreur ici
-        }
-      });
-    } else {
-      console.error("this.helpService.userId is undefined"); // Afficher une erreur dans la console
-    }
-  }
+  // private findById() {
+  //   if (this.helpService.userId) {
+  //     this.adresseService.findAll1(this.helpService.userId).subscribe({
+  //       next: (adresses) => {
+  //         this.adresse = adresses.filter((adresse) => adresse.utilisateurId === this.helpService.userId);
+  //       },
+  //       error: (err) => {
+  //         console.error(err);
+  //       }
+  //     });
+  //   } else {
+  //     console.error("this.helpService.userId is undefined");
+  //   }
+  // }
 
-  private getAllTypeAdresse() { // étape 2
-    this.adresseService.findAll1().subscribe({
-      next: (data) => {
-        this.typesAdresse = data;
+  private findById() {
+    this.adresseService.findAll1(this.helperService.userId).subscribe({
+      next: (adresses) => {
+        if (this.selectedUser) {
+          this.adresse = adresses.filter((adresse) => adresse.utilisateurId === this.selectedUser?.idUtilisateur
+          );
+        } else {
+          this.adresse = adresses.filter((adresse) => adresse.utilisateurId === this.helperService.userId);
+        }
       },
       error: (err) => {
         console.error(err);
+        // gérer le cas d'erreur ici
       }
     });
   }
+
+
+  private getAllTypeAdresse(): Observable<any> {
+    return new Observable((observer) => {
+      this.adresseService.findAll1().subscribe({
+        next: (data) => {
+          this.typesAdresse = data;
+          observer.next(data);
+          observer.complete();
+        },
+        error: (err) => {
+          console.error(err);
+          observer.error(err);
+        }
+      });
+    });
+  }
+
 }
